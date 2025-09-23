08dec9e8907e01e1f45dc4580d3f345c57b141e1
yossibank <yossibank@yahoo.co.jp>
2025-09-23 12:25:38 +0900

    Fixed Swift-Syntax version

--- Macro/URLMacro/Package.swift
+++ Macro/URLMacro/Package.swift
@@ -26,7 +26,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/swiftlang/swift-syntax.git",
-            from: "602.0.0"
+            from: "601.0.1"
         )
     ],
     targets: [
