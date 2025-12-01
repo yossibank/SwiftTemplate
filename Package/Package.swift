d4644b020aed1178e719d3baf4f0043b9c913c5c
yossibank <yossibank@yahoo.co.jp>
2025-12-02 01:29:49 +0900

    Fix KotlinMultiplatform Library

--- Package/Package.swift
+++ Package/Package.swift
@@ -335,7 +335,7 @@ let package = Package.package(
         ),
         .package(
             url: "https://github.com/yossibank/KotlinTemplate",
-            from: "1.1.6"
+            from: "1.1.7"
         ),
         .package(path: "../Macro/AutoInitMacro"),
         .package(path: "../Macro/BuilderMacro"),
